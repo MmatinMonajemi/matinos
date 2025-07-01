1e9e245070e6b0b0e845a0769058b4fda02bd161
M.Matin Monajemi <81869332+MmatinMonajemi@users.noreply.github.com>
2025-07-01 20:04:04 +0330

    Update kernel_entry.asm

--- matin-os/kernel_entry.asm
+++ matin-os/kernel_entry.asm
@@ -4,23 +4,24 @@ global init_pm
 
 init_pm:
     cli
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov esp, 0x9FC00
+    mov     ax, 0x10         ; Data segment selector (باید با GDT هماهنگ باشد)
+    mov     ds, ax
+    mov     es, ax
+    mov     fs, ax
+    mov     gs, ax
+    mov     ss, ax
+
+    mov     esp, 0x9FC00     ; Stack pointer (مطمئن شوید این آدرس آزاد است)
 
     sti
 
-    mov edi, 0xb8000
-    mov eax, 0x07454c4c
-    mov [edi], eax
-    mov eax, 0x074f5720
-    mov [edi+4], eax
-    mov eax, 0x074c4421
-    mov [edi+8], eax
+    mov     edi, 0xb8000
+    mov     eax, 0x07454c4c  ; 'HELL'
+    mov     [edi], eax
+    mov     eax, 0x074f5720  ; 'O W '
+    mov     [edi+4], eax
+    mov     eax, 0x074c4421  ; 'LD!'
+    mov     [edi+8], eax
 
 .halt:
     hlt

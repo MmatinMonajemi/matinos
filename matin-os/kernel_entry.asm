c107c9d5ffca76d031bc9f9837f695648b6e206d
M.Matin Monajemi <81869332+MmatinMonajemi@users.noreply.github.com>
2025-07-01 19:17:42 +0330

    Update kernel_entry.asm

--- matin-os/kernel_entry.asm
+++ matin-os/kernel_entry.asm
@@ -3,16 +3,16 @@ global init_pm
 extern main
 
 init_pm:
-    mov ax, 0x10       ; دیتا سگمنت
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    mov esp, 0x90000   ; استک
+    mov esp, 0x90000
 
     call main
 
-.halt:
+.hang:
     hlt
-    jmp .halt
+    jmp .hang

12748d0614204bdba7884ee941b2fe5e10dbb0d5
M.Matin Monajemi <81869332+MmatinMonajemi@users.noreply.github.com>
2025-07-01 19:40:06 +0330

    Update kernel_entry.asm

--- matin-os/kernel_entry.asm
+++ matin-os/kernel_entry.asm
@@ -1,18 +1,39 @@
+; kernel_entry.asm
 [BITS 32]
 global init_pm
-extern main
 
+section .text
 init_pm:
+    ; تنظیم سگمنت‌ها
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
+
+    ; تنظیم پشته
     mov esp, 0x90000
 
-    call main
+    ; نمایش پیام ساده
+    mov esi, message
+.print_loop:
+    lodsb
+    test al, al
+    jz .done
+    ; چاپ کاراکتر با اینترپت 0x10
+    mov ah, 0x0E
+    mov bh, 0
+    mov bl, 7
+    int 0x10
+    jmp .print_loop
 
-.hang:
+.done:
+    ; توقف
+    cli
+.hlt_loop:
     hlt
-    jmp .hang
+    jmp .hlt_loop
+
+section .data
+message db "Protected mode kernel started!", 0
